5a01526b8fea6bf12c927a1e8c72414783251091
Jess Y. <31145424+jcat1504@users.noreply.github.com>
2024-03-05 00:56:02 +0000

    this reference

--- other.js
+++ other.js
@@ -95,3 +95,15 @@ function ChildComponent() {
   return <div style={{ color: theme === 'light' ? 'black' : 'white' }}>Content</div>;
 }
 
+//this reference
+
+function myFunc() {
+  this.myVar = 0;
+  setTimeout(
+    () => { // this taken from surrounding, meaning myFunc here
+      this.myVar++;
+      console.log(this.myVar) // 1
+    },
+    0
+  );
+}
